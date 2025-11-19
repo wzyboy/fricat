3745ea316b7653da6c83f053403adc5ef2b8cc45
Zhuoyun Wei <wzyboy@wzyboy.org>
2025-11-18 22:19:20 -0800

    feat: concat rtsp_record

--- fricat/cli.py
+++ fricat/cli.py
@@ -1,6 +1,7 @@
 import click
 from pathlib import Path
-from fricat.concat import concat_to_mkv
+from fricat.concat import frigate
+from fricat.concat import rtsp_record
 
 
 @click.group()
@@ -11,5 +12,9 @@ def cli():
 @cli.command()
 @click.argument('src_root', type=click.Path(path_type=Path))
 @click.argument('dst_root', type=click.Path(path_type=Path))
-def concat(src_root: Path, dst_root: Path):
-    concat_to_mkv(src_root, dst_root)
+@click.option('--layout', default='frigate')
+def concat(src_root: Path, dst_root: Path, layout: str):
+    if layout == 'frigate':
+        frigate(src_root, dst_root)
+    elif layout == 'rtsp_record':
+        rtsp_record(src_root, dst_root)
--- fricat/concat.py
+++ fricat/concat.py
@@ -6,7 +6,27 @@ from pathlib import Path
 from tempfile import NamedTemporaryFile
 
 
-def concat_to_mkv(src_root: Path, dst_root: Path) -> None:
+def concat_to_mkv(src_files: list[Path], dst_file: Path) -> None:
+    with NamedTemporaryFile('wt', encoding='utf-8') as list_file:
+        for r in src_files:
+            list_file.write(f"file '{r}'\n")
+        list_file.flush()
+        _input = shlex.quote(list_file.name)
+        _output = shlex.quote(str(dst_file))
+        _ffmpeg = f'''
+            ffmpeg -hide_banner -loglevel warning
+            -f concat -safe 0
+            -i {_input}
+            -c copy
+            {_output}
+        '''
+        ffmpeg = shlex.split(_ffmpeg, comments=True)
+        print(shlex.join(ffmpeg))
+        dst_file.parent.mkdir(parents=True, exist_ok=True)
+        subprocess.run(ffmpeg, check=True)
+
+
+def frigate(src_root: Path, dst_root: Path) -> None:
     # /media/frigate/recordings/2025-11-18/14/CAM2/56.31.mp4
     #                                      %H      %M %S
     # /media/frigate/archive/2025-11-18/14_CAM2.mp4
@@ -31,24 +51,28 @@ def concat_to_mkv(src_root: Path, dst_root: Path) -> None:
                 raise AssertionError(f'Invalid recording: {r}') from None
 
         dst_dir = dst_root / date_str
-        out_file = dst_dir / f'{hour_str}_{cam_name}.mkv'
-        if out_file.exists():
+        dst_file = dst_dir / f'{hour_str}_{cam_name}.mkv'
+        if dst_file.exists():
             continue
+        concat_to_mkv(recordings, dst_file)
 
-        with NamedTemporaryFile('wt', encoding='utf-8') as list_file:
-            for r in recordings:
-                list_file.write(f"file '{r}'\n")
-            list_file.flush()
-            _input = shlex.quote(list_file.name)
-            _output = shlex.quote(str(out_file))
-            _ffmpeg = f'''
-                ffmpeg -hide_banner -loglevel warning
-                -f concat -safe 0
-                -i {_input}
-                -c copy
-                {_output}
-            '''
-            ffmpeg = shlex.split(_ffmpeg, comments=True)
-            print(shlex.join(ffmpeg))
-            dst_dir.mkdir(parents=True, exist_ok=True)
-            subprocess.run(ffmpeg, check=True)
+
+def rtsp_record(src_root: Path, dst_root: Path) -> None:
+    # /media/public/NVR/2024-11-30/CAM1_2024-11-30_00-00-01.mkv
+    # /media/public/NVR/2024-11-30/00_CAM1.mkv
+
+    def group_key(p: Path) -> tuple[str, str, str]:
+        cam_name, date_str, time_str = p.stem.split('_')
+        hour_str = time_str.split('-')[0]
+        return (date_str, hour_str, cam_name)
+
+    recordings = sorted(src_root.rglob('*.mkv'))
+    for key, recordings in itertools.groupby(recordings, key=group_key):
+        date_str, hour_str, cam_name = key
+        recordings = list(recordings)
+
+        dst_dir = dst_root / date_str
+        dst_file = dst_dir / f'{hour_str}_{cam_name}.mkv'
+        if dst_file.exists():
+            continue
+        concat_to_mkv(recordings, dst_file)
